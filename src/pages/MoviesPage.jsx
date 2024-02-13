b00532a90a22bec5722d58d0a02e058cdef9b497
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-02-13 11:20:41 +0200

    Update MoviesPage.jsx

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -10,35 +10,35 @@ const MoviesPage = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const controller = new AbortController();
-
-  const [params, setParams] = useSearchParams();
-  const query = params.get("query") ?? "";
+  
+  const [params, setParams] = useSearchParams();  
 
   const changeParams = (newParams) => {
     params.set("query", newParams);
     setParams(params);
   };
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        setLoading(true);
-        try {
-          setError(false);
-          const response = await searchMoviesByKeyword({
-            keyword: query,
-            abortController: controller,
-          });
-          setSearchResults(response);
-        } catch (error) {
-          if (error.code !== "ERR_CANCELED") {
-            setError(true);
+    useEffect(() => {
+      const controller = new AbortController();
+      const query = params.get("query") ?? "";
+      const fetchSearchResults = async () => {          
+        if (query) {
+          setLoading(true);
+          try {
+            setError(false);
+            const response = await searchMoviesByKeyword({
+              keyword: query,
+              abortController: controller,
+            });
+            setSearchResults(response);
+          } catch (error) {
+            if (error.code !== "ERR_CANCELED") {
+              setError(true);
+            }
+          } finally {
+            setLoading(false);
           }
-        } finally {
-          setLoading(false);
         }
-      }
     };
 
     fetchSearchResults();
